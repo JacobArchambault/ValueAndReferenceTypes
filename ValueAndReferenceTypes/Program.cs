5ef188dbb4440a5a64643cb26d7b1f81d71cb4dc
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-19 12:45:59 -0400

    Add ValueTypeAssignment().

--- ValueAndReferenceTypes/Program.cs
+++ ValueAndReferenceTypes/Program.cs
@@ -10,6 +10,58 @@ namespace ValueAndReferenceTypes
     {
         static void Main(string[] args)
         {
+            ValueTypeAssignment();
+            Console.ReadLine();
+        }
+
+        struct Point
+        {
+            public int X;
+            public int Y;
+
+            // A custom constructor.
+            public Point(int XPos, int YPos)
+            {
+                X = XPos;
+                Y = YPos;
+            }
+
+            // Add 1 to the (X, Y) position.
+            public void Increment()
+            {
+                X++; Y++;
+            }
+
+            // Subtract 1 from the (X, Y) position.
+            public void Decrement()
+            {
+                X--; Y--;
+            }
+
+            // Display the current position.
+            public void Display()
+            {
+                Console.WriteLine("X = {0}, Y = {1}", X, Y);
+            }
+        }
+
+        // Assigning two intrinsic value types results in two independent variables on the stack.
+        static void ValueTypeAssignment()
+        {
+            Console.WriteLine("Assigning value types \n");
+
+            Point p1 = new Point(10, 10);
+            Point p2 = p1;
+
+            // Print both points.
+            p1.Display();
+            p2.Display();
+
+            // Change p1.X and print again. p2.X is not changed.
+            p1.X = 100;
+            Console.WriteLine("\n Changed p1.X\n");
+            p1.Display();
+            p2.Display();
         }
     }
 }
