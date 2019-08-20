72f9415d7df4b148cbc7974feefa37005b46d5f7
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-20 11:28:43 -0400

    Add PointRef.cs

--- ValueAndReferenceTypes/Program.cs
+++ ValueAndReferenceTypes/Program.cs
@@ -65,38 +65,6 @@ namespace ValueAndReferenceTypes
             p2.Display();
         }
 
-        // Classes are always reference types.
-        class PointRef
-        {
-            public int X;
-            public int Y;
-
-            // A custom constructor.
-            public PointRef(int XPos, int YPos)
-            {
-                X = XPos;
-                Y = YPos;
-            }
-
-            // Add 1 to the (X, Y) position.
-            public void Increment()
-            {
-                X++; Y++;
-            }
-
-            // Subtract 1 from the (X, Y) position.
-            public void Decrement()
-            {
-                X--; Y--;
-            }
-
-            // Display the current position.
-            public void Display()
-            {
-                Console.WriteLine("X = {0}, Y = {1}", X, Y);
-            }
-        }
-
         static void ReferenceTypeAssignment()
         {
             Console.WriteLine("Assigning reference types\n");
