073df591315fc186f55e82e3216edb65eafa2a40
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-20 11:58:25 -0400

    Add ValueTypeContainingRefType() method.

--- ValueAndReferenceTypes/Program.cs
+++ ValueAndReferenceTypes/Program.cs
@@ -65,6 +65,7 @@ namespace ValueAndReferenceTypes
         {
             ValueTypeAssignment();
             ReferenceTypeAssignment();
+            ValueTypeContainingRefType();
             Console.ReadLine();
         }
 
@@ -106,6 +107,25 @@ namespace ValueAndReferenceTypes
             Console.WriteLine();
         }
 
+        static void ValueTypeContainingRefType()
+        {
+            // Create the first Rectangle.
+            Console.WriteLine("-> Creating r1");
+            Rectangle r1 = new Rectangle("First Rect", 10, 10, 50, 50);
 
+            // Now assign a new Rectangle to r1.
+            Console.WriteLine("-> Assigning r2 to r1");
+            Rectangle r2 = r1;
+
+            // Change some values of r2.
+            Console.WriteLine("-> Changing values of r2");
+            r2.RectInfo.InfoString = "This is new info!";
+            r2.RectBottom = 4444;
+
+            // Print values of both rectangles.
+            r1.Display();
+            r2.Display();
+            Console.WriteLine();
+        }
     }
 }
